5577c084866bdc27de446fc6bb6566e4f73225bd
Giadaspt <saputo.giada@gmail.com>
2021-11-24 13:53:48 +0100

    components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <div >
-
+  <div>
+    <Header/>
+    <Main/>
+    <InMain/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-
+import Header from "./components/Header.vue"
+import Main from "./components/Main.vue"
+import InMain from "./components/InMain.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Main,
+    InMain,
+    Footer,
   }
 }
 </script>
---
+++ src/components/InMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+ name: "InMain",
+}
+</script>
+
+<style>
+
+</style>
