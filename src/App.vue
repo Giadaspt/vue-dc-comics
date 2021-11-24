7b0acb036a5c0675b1f152e18693c3ea41a94c92
Giadaspt <saputo.giada@gmail.com>
2021-11-24 15:08:06 +0100

    in progress

--- src/App.vue
+++ src/App.vue
@@ -26,4 +26,7 @@ export default {
 
 <style lang="scss">
 
+@import "./assets/style/var.scss";
+@import "./assets/style/general.scss";
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,90 @@
 <template>
-  <header></header>
+  <header class="container">
+    <div class="image">
+      <a href="#">
+      <img src="../assets/img/dc-logo.png" alt="Dc Logo">
+      </a>
+    </div>
+    <nav>
+      <ul>
+        <li v-for="(item, index) in menu" :key="index">
+          <a :href="item.url"> {{item.nav}} </a>
+        </li>
+      </ul>
+    </nav>
+
+    
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+
+  data(){
+    return {
+      menu: [
+        {
+          url: '/characters',
+          nav: 'characters',
+        },
+        {
+          url: '/comics',
+          nav: 'comics',
+        },
+        {
+          url: '/movies',
+          nav: 'movies',
+        },
+        {
+          url: '/tv',
+          nav: 'tv',
+        },
+        {
+          url: '/games',
+          nav: 'games',
+        },
+        {
+          url: '/collectibles',
+          nav: 'collectibles',
+        },
+        {
+          url: '/videos',
+          nav: 'videos',
+        },
+        {
+          url: '/fans',
+          nav: 'fans',
+        },
+        {
+          url: '/news',
+          nav: 'news',
+        },
+        {
+          url: '/shop',
+          nav: 'shop',
+        },
+      ]
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/style/mixin.scss";
 
+  header{
+    @include position();
+    height: 80px;
+
+    .image {
+      width: 80px;
+      height: 80px;
+    }
+
+    a{
+      @include position();
+      color: $textHeadColor;
+    }
+  }
 </style>
